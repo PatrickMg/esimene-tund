b886aab53564c2b4044fe648a14fed46854b5781
patrick <PatrickMg@users.noreply.github.com>
2016-02-01 13:08:43 +0200

    kell

---
+++ clock.js
@@ -0,0 +1,38 @@
+ window.onload=function clock(){
+
+   var clock = document.getElementById("clock");
+
+writeDate();
+
+
+   window.setInterval(function(){
+     // iga ooteaja järel käivitatakse
+      writeDate();
+
+   },100);
+
+ };
+
+function writeDate(){
+
+  var today = new Date();
+
+  var hours = today.getHours();
+  var minutes = setZeroBefore(today.getMinutes());
+  var seconds = today.getSeconds();
+
+  second = setZeroBefore(seconds);
+
+  clock.innerHTML = setZeroBefore(hours) + ":" + minutes + ":" + second;
+}
+
+//lisab nulli kui arv on kümnest väiksem
+
+function setZeroBefore(number){
+
+  if(number < 10){
+    number = "0" + number;
+  }
+  return number;
+
+}
